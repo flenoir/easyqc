04af499e0805c236169866566ef2fcfe5ba3412a
Fabien Lenoir <flenoir@gmail.com>
2019-12-28 17:48:18 +0100

    added main page after login

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, MainPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('main', MainPageView.as_view(), name="main")
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class MainPageView(TemplateView):
+    template_name = 'main.html'
